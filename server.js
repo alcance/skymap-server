0d5d1c2391a22ced5b1cb9c633f974235e00841b
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-29 20:07:57 -0800

    Syntax fix for server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express();
 
 // Main route
-app.get('/', (req, res) => res.send('Hello Skycatch!'))
+app.get('/', (req, res) => res.send('Hello Skycatch!'));
 
 // Listen on port 8001
-app.listen(8001, () => console.log('Server is now live: http://localhost:8001/'))
+app.listen(8001, () => console.log('Server is now live: http://localhost:8001/'));
