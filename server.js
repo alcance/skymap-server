cabe01e820dc6fa553bed2be4233e89d4c6552f5
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-29 20:07:21 -0800

    Add main route and start express server

--- server.js
+++ server.js
@@ -2,4 +2,8 @@ import express from 'express'
 
 const app = express();
 
-console.log(app);
+// Main route
+app.get('/', (req, res) => res.send('Hello Skycatch!'))
+
+// Listen on port 8001
+app.listen(8001, () => console.log('Server is now live: http://localhost:8001/'))
