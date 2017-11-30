6ba931e1c2e6bc5a868d9849aadc7421c12ce238
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-29 22:42:00 -0800

    Locations GET and POST to Redis server

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
-import express from 'express'
+import express from 'express';
+import redis from 'redis';
+import bodyParser from 'body-parser';
+import uuid from 'uuid';
 
 const app = express();
+// Create Redis client
+const client = redis.createClient();
+client.on('connect',  () => console.log('Connected to Redis...'))
 
 // Setup constants
 const PORT = 8001;
 const HOST = '0.0.0.0';
 
+// bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 // Main route
-app.get('/', (req, res) => res.send('Hello Skycatch!'));
+app.get('/', (req, res, next) => res.send('Hello Skycatch!'));
+
+// Get all locations from Redis server
+app.get('/locations', (req, res, next) => {
+  client.keys('*', (err, obj) => res.json(obj));
+})
+
+// Post a new location to Redis server
+app.post('/locations/add', (req, res, next) => {
+  let uid = uuid.v1();
+  let lat = req.body.lat;
+  let lng = req.body.lng;
+  
+  client.hmset(uid, [
+    'lat', lat,
+    'lng', lng
+  ], (err) => {
+    if (err) console.log(err, status);
+    else res.json(uid);
+  });
+});
 
 // Listen on port 8001
 app.listen(PORT, HOST);
