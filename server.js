fa027513e2ea9d44ff7b832b05b325d1551c3c67
Ivan Martinez <alcance.enlinea@gmail.com>
2017-11-29 21:12:13 -0800

    Dockerize application

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from 'express'
 
 const app = express();
 
+// Setup constants
+const PORT = 8001;
+const HOST = '0.0.0.0';
+
 // Main route
 app.get('/', (req, res) => res.send('Hello Skycatch!'));
 
 // Listen on port 8001
-app.listen(8001, () => console.log('Server is now live: http://localhost:8001/'));
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
